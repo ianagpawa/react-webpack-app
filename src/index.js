df57e03e6d19fc89fa8f469f5b20d78b510f5472
Ian Agpawa <agpawaji@gmail.com>
2025-03-16 17:40:08 -0400

    feat: base webpack dev config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import "./style.css";
 
 function NavigationBar() {
   // TODO: Actually implement a navigation bar
-  return <h1>Hello from React!</h1>;
+  return <h1 class='hello'>Hello from React!</h1>;
 }
 
 const domNode = document.getElementById('navigation');
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: "defaults",
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/index.html', to: path.resolve(__dirname, 'dist')}
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        })
+    ]
+};
